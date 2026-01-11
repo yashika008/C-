dc844fcfdf0c06652263446eb77d440debcbfe38
yashika008 <93548785+yashika008@users.noreply.github.com>
2026-01-11 21:31:31 +0530

    pushed changes

--- DP/min_cost_climbing_stairs.cpp
+++ DP/min_cost_climbing_stairs.cpp
@@ -1,65 +1,50 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
+/*
+State Space
+===========
+S = {i | 0<=i<n}
+i is the step
+
+Interpretation
+==============
+dp[i] = minimum cost to reach the ith step
+
+Recursive relation
+==================
+dp[i] = min(dp[i-1], dp[i-2]) + nums[i]
+
+Base case
+=========
+dp[0] = nums[0]
+dp[1] = nums[1]
+
+End case
+========
+dp[n] = next to last
+ 
+*/
 class Solution {
-private:
-    int helper_recursion(vector<int>& cost, int index, int n){
-        if(index == 0) return cost[0];
-        if(index == 1) return min(cost[0], cost[1]);
-
-        return cost[index] + min(helper_recursion(cost, index-1,n),helper_recursion(cost, index-2,n));
-    }
-    int helper_TD(vector<int>& cost, vector<int>& dp, int index){
-        if(index<0)return 0;
-        //from step 0 there is only one way
-        if(index == 0) return cost[0];
-        //from step 1 
-        if(index == 1) return cost[1];
-        if(dp[index]!=INT_MAX) return dp[index];
-        return dp[index] = cost[index]+min(helper_TD(cost, dp, index-1),helper_TD(cost, dp, index-2));
-    }
-public:
-    int minCostClimbingStairs_recursion(vector<int>& cost) {
-        int n = cost.size();
-        return min(helper_recursion(cost, n-1,n), helper_recursion(cost, n-2,n));
-    }
-
-    int minCostClimbingStairs_top_down(vector<int>& cost) {
-        vector<int> dp(cost.size()+1,INT_MAX);
-        return min(helper_TD(cost, dp, cost.size()-2), helper_TD(cost, dp, cost.size()-1));
-    }
-    //bottoms up iterative
-
-    int minCostClimbingStairs_BUI(vector<int>& cost) {
-        vector<int> dp(cost.size(),-1);
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-        for(int i = 2;i < cost.size();i++){
-                dp[i] = cost[i]+min(dp[i-1], dp[i-2]);
+    public:
+        int minCostClimbingStairs(vector<int>& nums) {
+            int n = nums.size();
+            vector<int> dp(n, INT_MAX);
+            dp[0] = nums[0];
+            dp[1] = nums[1];
+            for(int i = 2;i < n;i++){
+                dp[i] = min(dp[i-1], dp[i-2]) + nums[i];
+            }
+            
+            return dp[n-1];
         }
-        return min(dp[cost.size()-1], dp[cost.size()-2]);
-    }
+    };
 
-    //bottoms up iterative space optimized
-
-    int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size(),-1);
-        int prev1 = cost[0];
-        int prev2 = cost[1];
-        int curr = prev2;
-        for(int i = 2;i < cost.size();i++){
-                curr = cost[i]+min(prev1, prev2);
-                prev1 = prev2;
-                prev2 = curr;
-        }
-        return min(prev1, prev2);
-    }
-
-};
 int main(){
-    Solution s;
-    vector<int> cost(4,1);
-    cout<<s.minCostClimbingStairs_recursion(cost);
-    return 0;
+vector<int> cost = {1, 3 ,4,3};
+Solution sol;
+cout<<sol.minCostClimbingStairs(cost);
+return 0;
 }
